3d3fa047c5cab6cab90a3cb7b7b4cf738c3a6537
PhilippeBurlet <philippe.burlet@viacesi.Fr>
2017-06-07 10:37:11 +0200

    Phase 2 maj

---
+++ helloworldmaven/helloworldmvc.model/src/main/java/org/helloworldmvc/model/bouchonnée.java
@@ -0,0 +1,14 @@
+package org.helloworldmvc.model;
+
+import org.helloworldmvc.Contract.IModel;
+
+public class bouchonnée {
+	public class Model implements IModel {
+		public String getHelloWorld() {
+		return null;
+		}	
+	}
+
+
+}
+
